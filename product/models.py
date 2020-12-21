c37decbdba2de8386e5981d746f5821985ff47aa
KamonratNg <kamonrat.nga@gmail.com>
2020-12-21 11:00:06 +0700

    news

--- product/models.py
+++ product/models.py
@@ -4,6 +4,7 @@ class AllProduct(models.Model):
 	product_name = models.CharField(max_length = 100)
 	product_price = models.CharField(max_length = 100)
 	product_detail = models.TextField(null = True ,blank = True)
+	product_photo = models.ImageField(upload_to = 'uploads/')
 
 	def __str__(self):
 		return self.product_name
